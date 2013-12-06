b7c120edd938781a8407eba2985dea2021b14c15
chad-feedhenry <chad.holmes@feedhenry.com>
2013-12-06 15:06:53 -0500

    Update stats.view.js
    Added getByFacebookUsername functions

--- client/default/scripts/views/stats.view.js
+++ client/default/scripts/views/stats.view.js
@@ -3,6 +3,28 @@
 App.View.StatsView = App.View.BaseView.extend({
 
   templateId: 'stats',
-  model: App.models.statsPage
-
+  model: App.models.statsPage,
+  
+  events: {
+    'click .get-fb-btn': 'getByFacebookUsername'
+  },
+  
+  initialize: function(){
+    _.blindAll(this,'getByFacebookUsername');
+  },
+  
+  getByFacebookUsername: function(){
+    var inputField = this.$el.find('#nameField');
+    var username = inputField.val();
+    $fh.act({
+      act: 'getByFacebookUsername',
+      req: {
+        username : username
+      }
+    }, fucntion(res){
+      alert(JSON.stringify(res));
+    }, function(msg, err){
+      alert(err.error);
+    });
+  },
 });
