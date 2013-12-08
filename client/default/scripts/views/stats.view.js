abf4e04112a272b3d8c5a06c21fa9c09e35f5628
chad-feedhenry <chad.holmes@feedhenry.com>
2013-12-07 20:25:06 -0500

    Update stats.view.js

--- client/default/scripts/views/stats.view.js
+++ client/default/scripts/views/stats.view.js
@@ -19,9 +19,13 @@ App.View.StatsView = App.View.BaseView.extend({
       }
     }, function(res){
       alert(JSON.stringify(res));
+      this.$el.find('.hidden').removeClass('hidden');
+      //this.$el.find('.response_content').removeClass('alert-error').addClass('alert-success').html('Response: ' + res.text);
+      this.$el.find('.response_content').removeClass('alert-error').addClass('alert-success').html('Response: ' + JSON.stringify(res));
       this.$el.find('.extra_response').removeClass('hidden');
+      //this.$el.find('.extra_response').removeClass('hidden');
       alert("Changing val...");
-      this.$el.find('.extra_response').text(JSON.stringify(res));
+      //this.$el.find('.extra_response').text(JSON.stringify(res));
     }, function(msg, err){
       alert(err.error);
     }
