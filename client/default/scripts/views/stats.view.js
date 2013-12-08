a41034fb35aaab42f6137258c77bf98b9be9ea25
chad-feedhenry <chad.holmes@feedhenry.com>
2013-12-07 19:57:28 -0500

    Update stats.view.js

--- client/default/scripts/views/stats.view.js
+++ client/default/scripts/views/stats.view.js
@@ -10,31 +10,18 @@ App.View.StatsView = App.View.BaseView.extend({
   },
   
   getByFacebookUsername: function(){
-    //alert('TEST!');
     var inputField = this.$el.find('#nameField');
-    //alert('INPUT FIELD = ' + inputField);
     var username = inputField.val();
-    alert('Username = ' + username);
-
-
     $fh.act({
       act: 'getByFacebookUsername',
       req: {
         username : username
       }
-      //act: 'getByFacebookUsername',
-      //req: {
-      //  username : username
-      //}
     }, function(res){
       alert(JSON.stringify(res));
     }, function(msg, err){
       alert(err.error);
     }
-    
-    
-    
-    
     );
   }
   
